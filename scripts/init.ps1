ca3eb15a2c184fa835fd5744942f484dcc346788
Ole Lynge Soerensen <olelynge@gmail.com>
2012-04-05 12:37:51 +0200

    Fix init script.

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,4 +1,4 @@
-#param($installPath, $toolsPath, $package, $project)
+param($installPath, $toolsPath, $package, $project)
 
 #Copy the -vsdoc-para file over the -vsdoc file
 #$projectFolder = Split-Path -Parent $project.FileName
