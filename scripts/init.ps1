73cd5773b702ca58c89e164e0b81e19dfb5ea5e0
Ole Lynge Soerensen <olelynge@gmail.com>
2012-04-05 13:56:00 +0200

    Update scripts.

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,24 +1,12 @@
 param($installPath, $toolsPath, $package, $project)
 
-#Copy the -vsdoc-para file over the -vsdoc file
-#$projectFolder = Split-Path -Parent $project.FileName
-#$projectFolder = $project.Properties.Item("FullPath").Value
-#$paraVsDocPath = Join-Path $toolsPath jquery-1.7.1-vsdoc-para.js
-#$vsDocPath = Join-Path $projectFolder Scripts\jquery-1.7.1-vsdoc.js
-#Copy-Item $paraVsDocPath $vsDocPath -Force
-
-"Nuget debug:" > C:\temp\nugetdebug.txt
-
-"InstallPath=$installPath" >> C:\temp\nugetdebug.txt
-"ToolsPath=$toolsPath" >> C:\temp\nugetdebug.txt
-"Package=$package" >> C:\temp\nugetdebug.txt
-"Project=$project" >> C:\temp\nugetdebug.txt
-
-#$installPath = "d:\projects\uncas\psen\scripts"
-
 $rootPath = "$installPath\..\.."
-"RootPath=$rootPath" >> C:\temp\nugetdebug.txt
 
-#$toolsPath = "d:\projects\uncas\psen\scripts"
+#"Nuget debug:" > C:\temp\nugetdebug.txt
+#"InstallPath=$installPath" >> C:\temp\nugetdebug.txt
+#"ToolsPath=$toolsPath" >> C:\temp\nugetdebug.txt
+#"Package=$package" >> C:\temp\nugetdebug.txt
+#"Project=$project" >> C:\temp\nugetdebug.txt
+#"RootPath=$rootPath" >> C:\temp\nugetdebug.txt
 
-Copy $toolsPath\init.ps1 $rootPath
+Copy $toolsPath\build.ps1 $rootPath
