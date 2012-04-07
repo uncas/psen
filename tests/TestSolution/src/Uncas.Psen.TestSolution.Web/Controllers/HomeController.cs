376c0585477989db8332e9daaa777099d2d8a4ea
Ole Lynge Soerensen <olelynge@gmail.com>
2012-04-07 22:15:36 +0200

    Add test solution.

---
+++ tests/TestSolution/src/Uncas.Psen.TestSolution.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Uncas.Psen.TestSolution.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hello Psen");
+        }
+    }
+}
---
+++ tests/TestSolution/src/Uncas.Psen.TestSolution.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Uncas.Psen.TestSolution.Web.MvcApplication" Language="C#" %>
