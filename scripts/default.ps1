76484b560eef0cb3dd09b70972faf9376bdf8769
Ole Lynge Soerensen <olelynge@gmail.com>
2012-04-13 19:47:56 +0200

    Remove manual NuGet install of psake and WinSCP since these are now explicit NuGet dependencies.

--- scripts/default.ps1
+++ scripts/default.ps1
@@ -64,7 +64,6 @@ if (!(Test-Path `$psenPath))`
 {`
     .nuget\nuget.exe install psen -o packages -version `$psenVersion`
 }`
-.nuget\nuget.exe install WinSCP -o packages -version 4.3.7`
 `
 & `$psenPath `$task"
 
--- src/psen.ps1
+++ src/psen.ps1
@@ -1,5 +1,4 @@
 $task = $args[0]
 $psakeVersion = "4.0.1.0"
-nuget install psake -version $psakeVersion -o packages
 Import-Module .\packages\psake.$psakeVersion\tools\psake.psm1
 & ".\packages\psake.$psakeVersion\tools\psake.cmd" ".\packages\psen.@PsenVersion@\tools\default.ps1" $task
