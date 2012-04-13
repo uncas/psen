2fc5bf2bb1e04980e8998d117810a6d82bf9c643
Ole Lynge Soerensen <olelynge@gmail.com>
2012-04-13 20:22:29 +0200

    Upgrade dependency to psake version 4.1.0.

--- src/psen.ps1
+++ src/psen.ps1
@@ -1,4 +1,4 @@
 $task = $args[0]
-$psakeVersion = "4.0.1.0"
+$psakeVersion = "4.1.0"
 Import-Module .\packages\psake.$psakeVersion\tools\psake.psm1
 & ".\packages\psake.$psakeVersion\tools\psake.cmd" ".\packages\psen.@PsenVersion@\tools\default.ps1" $task
